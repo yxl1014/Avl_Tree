a3ad169259245eb17d53fe988285354160114e12
yxl1014 <1627419257@qq.com>
2020-09-22 14:53:16 +0800

    master

--- src/AVL/Avltree.java
+++ src/AVL/Avltree.java
@@ -1,6 +1,13 @@
 package AVL;
 
 public class Avltree{
+    private TreeNode node;
+    public Avltree(){}
+    public int treehigh(TreeNode node) {
+        int left = node.leftnode == null ? -1 : node.leftnode.high;
+        int right = node.rightnode == null ? -1 : node.rightnode.high;
+        return Math.max(left, right) + 1;
+    }
 
 }
 
